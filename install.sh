fe02677d3f88e6734eb3f6a48d20ee9f8900a5e1
Kristoffer Andersson <kod.kristoff@gmail.com>
2024-12-11 14:46:53 +0100

    add zsh config

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ mkdir --parent "$HOME/.config/nvim"
 mkdir --parent "$HOME/.config/nvim/undo"
 ln -sf "$HOME/.dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 
+##################
+# zsh
+##################
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
