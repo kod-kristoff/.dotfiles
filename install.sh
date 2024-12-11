347b0aaab685f7f1f0e849ffe0219f9ede56a468
Kristoffer Andersson <kod.kristoff@gmail.com>
2024-12-11 22:38:14 +0100

    add zsh completion

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ ln -sf "$HOME/.dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 # zsh
 ##################
 mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/.dotfiles/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$HOME/.dotfiles/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
 ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
