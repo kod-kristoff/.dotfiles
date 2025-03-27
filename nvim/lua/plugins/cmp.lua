5903619b68a1c22631b3b576f2cee45b1aa1a88e
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-03-27 14:31:58 +0100

    refactor nvim plugins

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,53 +1,81 @@
-
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-buffer", -- source for text in buffer
-        "hrsh7th/cmp-path", -- source for file system paths
-        {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp",
-        },
-        "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim", -- vs-code like pictograms
-    },
-    config = function()
-        local cmp = require("cmp")
-        local lspkind = require("lspkind")
-        local luasnip = require("luasnip")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			-- install jsregexp (optional!).
+			build = "make install_jsregexp",
+		},
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+	},
+	config = function()
+		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		local luasnip = require("luasnip")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        })
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				-- Add tab support
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+			}),
+			sources = cmp.config.sources({
+				{ name = "path" }, -- file paths
+				{ name = "nvim_lsp", keyword_length = 3 }, -- from language server
+				{ name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
+				{ name = "nvim_lua", keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+				{ name = "buffer", keyword_length = 2 }, -- source current buffer
+				{ name = "vsnip", keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
+				{ name = "calc" },
+				-- { name = "nvim_lsp" },
+				-- { name = "luasnip" },
+				-- { name = "buffer" },
+				-- { name = "path" },
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			formatting = {
+				fields = { "menu", "abbr", "kind" },
+				format = function(entry, item)
+					local menu_icon = {
+						nvim_lsp = "λ",
+						vsnip = "⋗",
+						buffer = "Ω",
+						path = "🖫",
+					}
+					item.menu = menu_icon[entry.source.name]
+					return item
+				end,
+			},
+		})
 
-        vim.cmd([[
+		vim.cmd([[
       set completeopt=menuone,noinsert,noselect
       highlight! default link CmpItemKind CmpItemMenuDefault
     ]])
-    end,
+	end,
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,34 +1,35 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        require("mason").setup()
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
 
-        require("mason-lspconfig").setup({
-            automatic_installation = true,
-            ensure_installed = {
-                -- "cssls",
-                -- "eslint",
-                -- "html",
-                -- "jsonls",
-                -- "tsserver",
-                -- "pyright",
-                -- "tailwindcss",
-            },
-        })
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				-- "cssls",
+				-- "eslint",
+				-- "html",
+				-- "jsonls",
+				-- "tsserver",
+				-- "pyright",
+				-- "tailwindcss",
+				"rust-analyzer",
+			},
+		})
 
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                -- "prettier",
-                "stylua", -- lua formatter
-                "ruff", -- python formatter
-                -- "black", -- python formatter
-                -- "pylint",
-                -- "eslint_d",
-            },
-        })
-    end,
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				-- "prettier",
+				"stylua", -- lua formatter
+				"ruff", -- python formatter
+				-- "black", -- python formatter
+				-- "pylint",
+				-- "eslint_d",
+			},
+		})
+	end,
 }
